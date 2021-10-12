4e900357eb289064f73ccf157674f9ab3f7af786
IshchenkoMaksim <78483303+IshchenkoMaksim@users.noreply.github.com>
2021-10-12 12:30:16 +0300

    Update ind1.py

--- ind1.py
+++ ind1.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+def func(n, list_n):
+    if n == 0:
+        print(list_n[1], end='')
+        for i in list_n[2:]:
+            print(f'+{i}', end='')
+        print('')
+    else:
+        x = list_n[len(list_n) - 1]
+        if n >= x:
+            for i in range(max(1, x), n + 1):
+                func(n - i, list_n + [i])
+
 if __name__ == '__main__':
-
-    def func(n, list_n):
-        if n == 0:
-            print(list_n[1], end='')
-            for i in list_n[2:]:
-                print(f'+{i}', end='')
-            print('')
-        else:
-            x = list_n[len(list_n) - 1]
-            if n >= x:
-                for i in range(max(1, x), n + 1):
-                    func(n - i, list_n + [i])
-
-
     num = int(input('Введите число, которое хотите разложить на слагаемые: '))
     func(num, [0])
